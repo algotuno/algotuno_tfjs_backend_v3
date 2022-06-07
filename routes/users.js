030ca3284eb2b9dc57979d96e6625e7c9fae37b9
sayyidkhan <sayyidkhan92@hotmail.com>
2022-06-07 19:41:56 +0800

    add tfjs

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send({message: 'Hello World'});
 });
 
+// POST method route
+router.post('/', (req, res) => {
+  res.send({
+    data : req.body,
+  })
+});
+
 module.exports = router;
