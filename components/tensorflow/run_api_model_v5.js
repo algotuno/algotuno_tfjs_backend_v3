762f48936571d94f342561ff103f870f8c6f766e
sayyidkhan <sayyidkhan92@hotmail.com>
2022-07-23 23:01:53 +0800

    update api endpoint for tensorflow api

--- components/tensorflow/run_api_model_v5.js
+++ components/tensorflow/run_api_model_v5.js
@@ -1,5 +1,8 @@
+const {calculate_mape_between_historical_and_forecast_price} = require("./toolsets/forecast_probability_util");
+const {calculate_mse_between_historical_and_forecast_price} = require("./toolsets/mse_util");
+
 const {loadLayersModel} = require("@tensorflow/tfjs-node");
-const {forecast, get_stock_data, predict} = require("./toolsets/core_ai_v4.js");
+const {forecast, get_stock_data, predict} = require("./toolsets/core_ai_v5.js");
 
 
 async function run_api_model(stock_data, _tf_model_dir) {
@@ -12,6 +15,7 @@ async function run_api_model(stock_data, _tf_model_dir) {
     const no_of_trading_days_in_a_year = 260; // no of trading days in a year
     const no_of_trading_days_in_a_year_multiplier = 2; // no of years / batches to cover
     const days_range = 5; // the number of days range to compute in
+    const _display_in_epoch_time = true;
 
 
     /*** program logic ***/
@@ -59,20 +63,86 @@ async function run_api_model(stock_data, _tf_model_dir) {
             let day7 = result.output_ys_list[6]; // day 7
             let day30 = result.output_ys_list[29]; // day 30
             function _convert_array_to_obj(_day_array) {
-                const _obj = {};
-                console.log(_day_array);
-                const epoch_time = _day_array[1].getTime().toString();
-                _obj[epoch_time] = _day_array[0];
+                const _obj = {
+                    "epoch_time": '',
+                    "price": 0,
+                    "confidence_score": 0,
+                    "rate_of_error": 0,
+                };
+
+                let epoch_time;
+                if (_display_in_epoch_time) {
+                    epoch_time = _day_array[1].getTime().toString();
+                } else {
+                    epoch_time = _day_array[1].toISOString();
+                }
+
+                _obj['epoch_time'] = epoch_time;
+                _obj['price'] = _day_array[0];
                 return _obj;
             }
+
+            const historical_list = stock_dataset.validation.raw_ys_list;
+            const predict_list = validation_data_result;
 
             // obj format
             day1 = _convert_array_to_obj(day1);
             day7 = _convert_array_to_obj(day7);
             day30 = _convert_array_to_obj(day30);
 
-            // 3 days prediction
-            return [day1, day7, day30];
+            return {
+                "historical_list": historical_list,
+                "predict_list": predict_list,
+                // (still need to update confidence score because it is 0)
+                "forecast_list": [day1, day7, day30], // 3 days prediction
+            }
+        }).then(_result_entity => {
+            const historical_list = _result_entity.historical_list;
+            const predicted_list = _result_entity.predict_list;
+            const forecast_list = _result_entity.forecast_list;
+            // 4. get the mape score against the testing dataset
+            // get the confidence score
+            const mape_entity = calculate_mape_between_historical_and_forecast_price(
+                historical_list,
+                predicted_list,
+                forecast_list
+            );
+
+            // update confidence score
+            const day1 = forecast_list[0];
+            day1.confidence_score = mape_entity.mape_1;
+            const day7 = forecast_list[1];
+            day7.confidence_score = mape_entity.mape_7;
+            const day30 = forecast_list[2];
+            day30.confidence_score = mape_entity.mape_30;
+
+            return {
+                "historical_list": historical_list,
+                "predict_list": predicted_list,
+                // (still need to update loss function because it is 0)
+                "forecast_list": forecast_list, // 3 days prediction
+            }
+        }).then(_result_entity => {
+            const historical_list = _result_entity.historical_list;
+            const predicted_list = _result_entity.predict_list;
+            const forecast_list = _result_entity.forecast_list;
+            // 4. get the mape score against the testing dataset
+            // get the confidence score
+            const mse_entity = calculate_mse_between_historical_and_forecast_price(
+                historical_list,
+                predicted_list,
+                forecast_list
+            );
+
+            // update confidence score
+            const day1 = forecast_list[0];
+            day1.rate_of_error = mse_entity.mse_1;
+            const day7 = forecast_list[1];
+            day7.rate_of_error = mse_entity.mse_7;
+            const day30 = forecast_list[2];
+            day30.rate_of_error = mse_entity.mse_30;
+
+            return forecast_list;
         });
 
     return predict_result;
--- components/tensorflow/toolsets/core_ai_v5.js
+++ components/tensorflow/toolsets/core_ai_v5.js
@@ -184,7 +184,7 @@ const nn_model = (training_data, labelled_data) => tidy(() => {
     return model;
 });
 
-async function train_model(xs, ys, iterations, _model, print_training_log) {
+async function train_model(xs, ys, iterations, _model, print_training_log, company_name) {
     /**
      *
      * used to train the model with the training dataset
@@ -225,7 +225,9 @@ async function predict(_model, _training_data, _labelled_data) {
 
 async function predict_type2(_model, _training_data, _labelled_data) {
     const _prediction = await _model.predict(_training_data);
-    let _predict = _prediction.arraySync().map(x => x[0]);
+    let _predict = _prediction.arraySync().map(x => {
+        return x[0];
+    });
 
     const latest_price = _predict.slice(-1)[0];
     const latest_date = _labelled_data[1];
